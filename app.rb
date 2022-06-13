21fe829b9ee011aa800a6b494bbecde71bce63f9
MissDine <enidnyatichi1212@gmail.com>
2022-06-13 17:42:59 +0300

    Feature edit

--- app.rb
+++ app.rb
@@ -1,2 +1,10 @@
 # SQL-Structures querry language-language used to comuunicate with data bases
 # It is used by many different database systems such as MySQL, PostgreSQL
+#  SQL systems like Postgres, or MySQ
+# A database is a place where you can store your data
+# sql lite is a lighter version of sql
+# Data types in sql-null,interger,text(string),real,blob
+
+# Primary & foreign key
+# primary-a unique key eg usernames, passwords should be unique
+# foreign-
